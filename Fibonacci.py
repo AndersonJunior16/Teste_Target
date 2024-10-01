6c4a105567daa3ad20123d69cd9e27dce0d8735a
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-10-01 16:58:19 -0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,4 @@
+#Função que gera o Fibonacci através no número digitado    
 def gerar_fibonacci(num):
     fibonacci = [0,1]   
 
@@ -7,6 +8,7 @@ def gerar_fibonacci(num):
 
     return fibonacci
 
+#Função que verifica se exite o número digitado dentro de Fibonacci
 def verificar(num):
     fibonacci = gerar_fibonacci(num)
     if num in fibonacci:
@@ -14,7 +16,7 @@ def verificar(num):
     else:
         return f"O número {num} não pertence a Fibonacci!"
 
-
+#Principal - Digitar o número e chamar a função
 num = int(input("Digite um valor para vermos se pertence e a Fibonacci: "))
 resultado = verificar(num)
 print(resultado)
